31b6d4ad80f9111b18e0df0eab3fd48db7312b1a
prepvioai <prepvio.ai@gmail.com>
2026-02-10 01:25:18 +0530

    casing

--- Backened/Routes/paymentRoute.js
+++ Backened/Routes/paymentRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import Razorpay from "razorpay";
 import crypto from "crypto";
 import { PLANS } from "../config/plans.js";
-import { User } from "../models/User.js";
+import { User } from "../Models/User.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import Notification from "../Models/Notification.js";
 import { io } from "../server.js";
--- Backened/Routes/userRoutes.js
+++ Backened/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { User } from "../models/User.js";
+import { User } from "../Models/User.js";
 import { verifyToken, isAdmin } from "../middleware/authMiddleware.js";
 import { sendCourseStartedNotification, sendCourseCompletedNotification } from "../Utils/notificationHelper.js";
 import { deduplicateCourseProgress } from "../Utils/courseHelper.js";
